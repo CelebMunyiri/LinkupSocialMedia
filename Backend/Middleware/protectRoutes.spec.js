2a8cea480e692d0684d7310967ef837a51b84177
David Munyiri <davidmumyiri2019@gmail.com>
2023-09-05 17:39:57 +0300

    wrote tests for middle and all the tests for the middleware passed

---
+++ Backend/Middleware/protectRoutes.spec.js
@@ -0,0 +1,57 @@
+import jwt from 'jsonwebtoken';
+import { verifyToken } from './protectRoutes';
+
+jest.mock('jsonwebtoken');
+
+
+describe('Checking Token', () => {
+    it('should get the token', async () => {
+        jwt.verify.mockReturnValue({ UserID: '1234' });
+
+        const req = {
+            headers: { token: 'valid_token' },
+        };
+        const res = {
+            status: jest.fn().mockReturnThis(),
+            json: jest.fn().mockReturnThis(),
+        };
+        const next = jest.fn();
+
+        await verifyToken(req, res, next);
+
+        expect(next).toHaveBeenCalled();
+    });
+
+    it('Show 401 if token is missing', async () => {
+        const req = { headers: {} };
+        const res = {
+            status: jest.fn(() => res),
+            json: jest.fn()
+        };
+        const next = jest.fn();
+        await verifyToken(req, res, next),
+
+            expect(res.status).toHaveBeenCalledWith(401);
+        expect(res.json).toHaveBeenCalledWith({ message: 'You are not allowed to be seeing This, provide a token' })
+    });
+
+    it('should handle invalid token', async () => {
+        const req = {
+          headers: { token: 'invalid_token' },
+        };
+        const res = {
+          json: jest.fn(),
+        };
+        const next = jest.fn();
+      
+        try {
+          await verifyToken(req, res, next);
+        } catch (error) {
+          expect(error.message).toBe('Invalid token');
+        }
+      
+        expect(res.json).not.toHaveBeenCalled();
+        expect(next).toHaveBeenCalled();
+      });
+      
+});
